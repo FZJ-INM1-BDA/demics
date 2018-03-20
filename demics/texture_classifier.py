38e3f25fdd6804e959ee0feaac4a12e2c7cf8170
Sarah <sa.haas@fz-juelich.de>
2018-03-20 11:09:11 +0100

    removed old TODO

--- demics/texture_classifier.py
+++ demics/texture_classifier.py
@@ -80,7 +80,6 @@ class TextureClassifier(object):
             Object of class TextureClassifier
         """
         import pickle
-        #TODO pickle.load does not work in python3...
         dictionary = pickle.load(open(filename,"rb"))
         obj = cls(dictionary["histogram_bins"], dictionary["numFeatures"])  # old names (from old classifiers)...
         #obj = cls(dictionary["num_histogram_bins"], dictionary["pca_dims"])
